94722ee447b618ad1b21cb3321f7efaa4b35b220
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-05 15:24:58 +0700

    logo Section done

--- src/components/sections/other/SkillSection.jsx
+++ src/components/sections/other/SkillSection.jsx
@@ -1,19 +1,60 @@
 import Section from '../../ui/section/Section';
 import Container from '../../ui/container/Container';
 import Grid from '../../ui/grid/Grid';
+import Logo from '../../ui/common/Logo';
+import reactLogo from '../../../assets/svg/logo/react.svg';
+import tailwindLogo from '../../../assets/svg/logo/tailwindcss.svg';
+import reduxLogo from '../../../assets/svg/logo/redux.svg';
+import postgresLogo from '../../../assets/svg/logo/postgresql.svg';
+import nodeLogo from '../../../assets/svg/logo/nodejs.svg';
+import npmLogo from '../../../assets/svg/logo/npm.svg';
+import htmlLogo from '../../../assets/svg/logo/html5.svg';
+import jsLogo from '../../../assets/svg/logo/javascript.svg';
+import gitLogo from '../../../assets/svg/logo/git.svg';
+import expressLogo from '../../../assets/svg/logo/express.svg';
+import cssLogo from '../../../assets/svg/logo/css3.svg';
+import gitHubLogo from '../../../assets/svg/logo/github.svg';
 
 export default function SkillSection() {
+  const logos = [
+    { src: reactLogo, alt: 'React Logo' },
+    { src: tailwindLogo, alt: 'Tailwind Logo' },
+    { src: reduxLogo, alt: 'Redux Logo' },
+    { src: postgresLogo, alt: 'PostgreSQL Logo' },
+    { src: nodeLogo, alt: 'Node.js Logo' },
+    { src: npmLogo, alt: 'npm Logo' },
+    { src: htmlLogo, alt: 'HTML Logo' },
+    { src: jsLogo, alt: 'JavaScript Logo' },
+    { src: gitLogo, alt: 'Git Logo' },
+    { src: expressLogo, alt: 'Express Logo' },
+    { src: cssLogo, alt: 'CSS Logo' },
+    { src: gitHubLogo, alt: 'GitHub Logo' },
+  ];
+
   return (
     <Section>
       <Container>
         <div className='w-9/12 mx-auto'>
           <h2 className='md:text-3xl text-xl font-semibold text-center'>
-            Unlock Your Potential with Our
+            Unlock Your Potential with My
             <br className='hidden md:block' />
-            Comprehensive Fullstack Development Stack
+            Fullstack Development Stack
           </h2>
-          <Grid cols='grid-cols-2' mdCols='md:grid-cols-3' gap='gap-6'>
-            {/* Grid items */}
+          <Grid
+            className='w-8/12 mx-auto mt-12'
+            cols='grid-cols-2'
+            lgCols='lg:grid-cols-3'
+            mdCols='md:grid-cols-4'
+            gap='gap-6'
+          >
+            {logos.map((logo, index) => (
+              <Logo
+                key={index}
+                src={logo.src}
+                alt={logo.alt}
+                className='w-24 h-24'
+              />
+            ))}
           </Grid>
         </div>
       </Container>
--- src/components/ui/common/Logo.jsx
+++ src/components/ui/common/Logo.jsx
@@ -1,9 +1,28 @@
-import Image from 'next/image';
+import PropTypes from 'prop-types';
+import LogoImage from '../../../assets/svg/logo/react.svg';
 
-export default function Logo() {
+/**
+ * A logo component with customizable image and alt text
+ *
+ * @param {Object} props
+ * @param {string} [props.src] - The source URL for the logo image
+ * @param {string} [props.alt] - The alt text for the logo image
+ * @param {string} [props.className] - Additional CSS classes for the logo
+ */
+export default function Logo({
+  src = LogoImage,
+  alt = 'Logo',
+  className = 'w-24 h-24',
+}) {
   return (
     <div>
-      <Image src={LogoImage} alt='Logo' />
+      <img className={className} src={src} alt={alt} />
     </div>
   );
 }
+
+Logo.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+};
