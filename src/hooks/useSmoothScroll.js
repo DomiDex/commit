c21c0c5c2c80ce6fa9f96c2655986729070337aa
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-05 17:40:04 +0700

    modify smooth scroll

--- src/hooks/useSmoothScroll.js
+++ src/hooks/useSmoothScroll.js
@@ -4,13 +4,13 @@ import Lenis from '@studio-freight/lenis';
 export function useSmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.7,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
       smooth: true,
       smoothTouch: false,
-      touchMultiplier: 3,
+      touchMultiplier: 2,
     });
 
     function raf(time) {
