acf23722e450e14fd38a22ebad666b022fc837a9
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-05 10:34:05 +0700

    image path changed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,14 @@ import ContentSectionCenter from '../components/sections/other/ContentSectionCen
 export default function About() {
   return (
     <>
-      <SideSection />
+      <SideSection
+        title='Who am I?'
+        description={`  This project involved creating a dynamic e-commerce platform tailored to the client's specifications. Utilizing cutting-edge technologies such as React for the front end and Node.js for the back end, we ensured a seamless user experience. 
+          The client required a robust system that could handle high traffic while maintaining fast load times and an intuitive interface.`}
+        image='/path/to/your/image.jpg'
+        imageAlt='Vision illustration'
+        titleAs='h1'
+      />
       <SideSectionReverse />
       <ContentSectionCenter />
     </>
