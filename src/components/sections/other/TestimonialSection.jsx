57548b429e9e638f19b10f00801eb1bd1ae29986
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-05 16:00:13 +0700

    Testimonial done

--- src/components/sections/other/TestimonialSection.jsx
+++ src/components/sections/other/TestimonialSection.jsx
@@ -1,19 +1,55 @@
 import Section from '../../ui/section/Section';
 import Container from '../../ui/container/Container';
 import TestimonialCard from '../../ui/common/TestimonialCard';
+import testimonialStar from '../../../assets/svg/testimonial/Stars.svg';
+import testimonialImage from '../../../assets/images/testimonial/test-profile.webp';
+import testimonialImage2 from '../../../assets/images/testimonial/testi-profile-01.webp';
 
 export default function TestimonialSection() {
+  const testimonials = [
+    {
+      starIcon: testimonialStar,
+      quote:
+        'Working with this developer was an absolute pleasure. Their technical expertise and attention to detail resulted in a product that exceeded our expectations.',
+      profileImage: testimonialImage,
+      authorName: 'Jane Smith',
+      authorTitle: 'CTO, Tech Corp',
+    },
+    {
+      starIcon: testimonialStar,
+      quote:
+        'Exceptional problem-solving skills and a great communicator. Delivered our project on time and with outstanding quality. Highly recommended!',
+      profileImage: testimonialImage2,
+      authorName: 'John Doe',
+      authorTitle: 'Founder, Digital Solutions',
+    },
+  ];
+
   return (
     <Section>
       <Container>
-        <div>
-          <h2 className='text-4xl font-bold'>Customer testimonials</h2>
-          <p className='mt-4'>
-            Here are some testimonials from my clients and colleagues.{' '}
-            <br className='hidden md:block' /> They highlight my skills and the
-            quality of my work.
-          </p>
-          <TestimonialCard />
+        <div className='flex flex-col space-y-12'>
+          <div className='flex flex-col space-y-4'>
+            <h2 className='text-4xl font-bold'>Customer testimonials</h2>
+            <p className='mt-4'>
+              Here are some testimonials from my clients and colleagues.{' '}
+              <br className='hidden md:block' /> They highlight my skills and
+              the quality of my work.
+            </p>
+          </div>
+          <div className='grid md:grid-cols-2 gap-8'>
+            {testimonials.map((testimonial, index) => (
+              <TestimonialCard
+                key={index}
+                starIcon={testimonial.starIcon}
+                quote={testimonial.quote}
+                profileImage={testimonial.profileImage}
+                authorName={testimonial.authorName}
+                authorTitle={testimonial.authorTitle}
+                className='bg-slate-800 p-6 rounded-lg'
+              />
+            ))}
+          </div>
         </div>
       </Container>
     </Section>
---
+++ src/components/ui/common/TestimonialCard.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import testimonialStar from '../../../assets/svg/testimonial/stars.svg';
+import testimonialImage from '../../../assets/images/testimonial/test-profile.webp';
+
+/**
+ * A testimonial card component with customizable content
+ *
+ * @param {Object} props
+ * @param {string} [props.starIcon] - The star rating icon source
+ * @param {string} [props.quote] - The testimonial quote
+ * @param {string} [props.profileImage] - The testimonial author's image
+ * @param {string} [props.authorName] - The author's name
+ * @param {string} [props.authorTitle] - The author's title/position
+ * @param {string} [props.className] - Additional CSS classes
+ */
+export default function TestimonialCard({
+  starIcon = testimonialStar,
+  quote = "The project exceeded our expectations, showcasing exceptional skill and dedication. We couldn't be happier with the results!",
+  profileImage = testimonialImage,
+  authorName = 'John Doe',
+  authorTitle = 'CEO, Company Name',
+  className = '',
+}) {
+  return (
+    <div className={`flex flex-col items-start space-y-4 ${className}`}>
+      <img className='w-36 h-auto' src={starIcon} alt='Testimonial star' />
+      <p className='text-lg italic'>&quot;{quote}&quot;</p>
+      <div className='flex items-center space-x-2'>
+        <img
+          className='w-12 rounded-full h-auto'
+          src={profileImage}
+          alt={`${authorName}'s profile`}
+        />
+        <div>
+          <h3 className='text-lg font-bold'>{authorName}</h3>
+          <p className='text-sm'>{authorTitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+TestimonialCard.propTypes = {
+  starIcon: PropTypes.string,
+  quote: PropTypes.string,
+  profileImage: PropTypes.string,
+  authorName: PropTypes.string,
+  authorTitle: PropTypes.string,
+  className: PropTypes.string,
+};
