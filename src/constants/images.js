8c1d2805ce5ba94e960d49420151238fd8686558
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-06 21:09:17 +0700

    constan image creaated

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,6 +3,16 @@ export const PLACEHOLDER_IMAGE =
   'data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA';
 
 export const IMAGES = {
+  about: {
+    mainAboutOne: {
+      src: '@assets/images/about/main-about-one@2x.webp',
+      alt: 'About main image',
+    },
+    mainAboutTwo: {
+      src: '@assets/images/about/main-about-two@2x.webp',
+      alt: 'About secondary image',
+    },
+  },
   logo: {
     src: '/assets/svg/logo/react.svg',
     alt: 'Logo',
--- vite.config.js
+++ vite.config.js
@@ -13,24 +13,16 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
+        assetFileNames: ({ name }) => {
+          const folderPath = name.split('/').slice(0, -1).join('/');
+          return `${folderPath}/[name]-[hash][extname]`;
         },
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
-    copyPublicDir: true,
-    emptyOutDir: true,
   },
   publicDir: 'public',
-  base: './',
+  base: '/',
 });
