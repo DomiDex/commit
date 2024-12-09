b753182df54b86f1946c284d576ec892a1d745ec
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-09 09:47:46 +0700

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@images': path.resolve(__dirname, './src/assets/images')
+    }
   },
-  assetsInclude: ['**/*.webp'],
   build: {
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-      },
-    },
-  },
+        manualChunks: undefined
+      }
+    }
+  }
 });
